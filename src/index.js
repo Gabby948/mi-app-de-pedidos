0557f7ca537062c06864dbb24830200b4bac51e1
Gabby948 <teffa08-94@hotmail.com>
2024-08-28 21:20:13 -0500

    adding more components and debugging

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './App.css';  
 import App from './App'; 
 
-// Obtén el elemento con id "root" del HTML
+
 const rootElement = document.getElementById('root');
 
-// Crea el root de React y renderiza el componente App dentro de este root
+
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />  {/* Renderiza el componente principal */}
+    <App />  {//depurar}
   </React.StrictMode>
 );
